76c5f8c80ae8fbee92c9300cbab8c8f7de2c82c9
Js Store <jsstoree2025@gmail.com>
2025-09-06 12:26:04 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,8 +1,6 @@
-// supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-// Use variáveis de ambiente da Vercel
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY!
+);
