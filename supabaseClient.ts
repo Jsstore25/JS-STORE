d5bc9ae680c0da192456562bb9ca378a45518a78
Js Store <jsstoree2025@gmail.com>
2025-09-06 11:55:21 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,8 +1,6 @@
-// supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-// Substitua pelos seus dados do Supabase
-const SUPABASE_URL = "https://byhqhcmqurjctyzlipra.supabase.co";
-const SUPABASE_KEY = "sb_publishable_3qYi0_suRsWOPvj9Z1PQCA_PvrUIOEH";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY!
+);
